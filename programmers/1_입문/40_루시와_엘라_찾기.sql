379aebfa3cb466a1948057f206b3053a33738010
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:28:28 +0900

    [프로그래머스] 입문 - 루시와 엘라 찾기 (#40)

---
+++ programmers/1_입문/40_루시와_엘라_찾기.sql
@@ -0,0 +1,13 @@
+-- 루시와 엘라 찾기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59046
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:28:28
+
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID ASC;
