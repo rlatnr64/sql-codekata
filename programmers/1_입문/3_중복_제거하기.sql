c376a3106ee6f1cbf052bf19ff431d408bb08793
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:20:45 +0900

    [프로그래머스] 입문 - 중복 제거하기 (#3)

---
+++ programmers/1_입문/3_중복_제거하기.sql
@@ -0,0 +1,9 @@
+-- 중복 제거하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59408
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:20:44
+
+SELECT
+    COUNT(DISTINCT NAME)
+FROM ANIMAL_INS;
