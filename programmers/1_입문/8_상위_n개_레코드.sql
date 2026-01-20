0861bd04e0723bfcd5f2a028fc23180fb9168c7a
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:21:39 +0900

    [프로그래머스] 입문 - 상위 n개 레코드 (#8)

---
+++ programmers/1_입문/8_상위_n개_레코드.sql
@@ -0,0 +1,11 @@
+-- 상위 n개 레코드
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59405
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:21:38
+
+SELECT
+    NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
