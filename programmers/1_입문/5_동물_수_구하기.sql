97a2362faaa0c622c2965b0df8664123fcd8bdd0
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:21:13 +0900

    [프로그래머스] 입문 - 동물 수 구하기 (#5)

---
+++ programmers/1_입문/5_동물_수_구하기.sql
@@ -0,0 +1,9 @@
+-- 동물 수 구하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59406
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:21:12
+
+SELECT
+    COUNT(*)
+FROM ANIMAL_INS;
