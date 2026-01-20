3bffa158bae7b1bca9249591acf6a480bd8b98c1
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:26:29 +0900

    [프로그래머스] 입문 - 흉부외과 또는 일반외과 의사 목록 출력하기 (#19)

---
+++ programmers/1_입문/19_흉부외과_또는_일반외과_의사_목록_출력하기.sql
@@ -0,0 +1,14 @@
+-- 흉부외과 또는 일반외과 의사 목록 출력하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/132203
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:26:28
+
+SELECT
+    DR_NAME,
+    DR_ID,
+    MCDP_CD,
+    DATE_FORMAT(HIRE_YMD, "%Y-%m-%d") as HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD IN("CS", "GS")
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
