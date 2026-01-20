675232a40cff427b56389c6021a2c70bf22a3fc3
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:22:26 +0900

    [프로그래머스] 입문 - 여러 기준으로 정렬하기 (#11)

---
+++ programmers/1_입문/11_여러_기준으로_정렬하기.sql
@@ -0,0 +1,12 @@
+-- 여러 기준으로 정렬하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59404
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:22:26
+
+SELECT
+    ANIMAL_ID,
+    NAME,
+    DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME ASC, DATETIME DESC;
