0fbed638e84dba6db5241e2ffc1dd45dddbbe2df
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:20:20 +0900

    [프로그래머스] 입문 - 역순 정렬하기 (#2)

---
+++ programmers/1_입문/2_역순_정렬하기.sql
@@ -0,0 +1,11 @@
+-- 역순 정렬하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59035
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:20:20
+
+SELECT
+    NAME,
+    DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
