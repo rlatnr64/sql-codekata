37c3d8496e2f701a64fd7187c58a0ce0dd0cc365
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:20:58 +0900

    [프로그래머스] 입문 - 동물의 아이디와 이름 (#4)

---
+++ programmers/1_입문/4_동물의_아이디와_이름.sql
@@ -0,0 +1,11 @@
+-- 동물의 아이디와 이름
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59403
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:20:58
+
+SELECT
+    ANIMAL_ID,
+    NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC;
