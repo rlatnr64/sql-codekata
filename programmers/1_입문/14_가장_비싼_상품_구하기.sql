3a820f5b61c6adc2ee866a4f3715e4a22bbc830d
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-20 11:24:21 +0900

    [프로그래머스] 입문 - 가장 비싼 상품 구하기 (#14)

---
+++ programmers/1_입문/14_가장_비싼_상품_구하기.sql
@@ -0,0 +1,9 @@
+-- 가장 비싼 상품 구하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131697
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 20. 11:24:21
+
+SELECT
+    MAX(PRICE) AS MAX_PEICE
+FROM PRODUCT;
