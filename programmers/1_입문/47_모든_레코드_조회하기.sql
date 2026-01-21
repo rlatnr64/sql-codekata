f9bdfa0c0ff96bb27b3e6877e884e5a06ed4680e
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-21 10:55:21 +0900

    [프로그래머스] 입문 - 모든 레코드 조회하기 (#47)

---
+++ programmers/1_입문/47_모든_레코드_조회하기.sql
@@ -0,0 +1,9 @@
+-- 모든 레코드 조회하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- 작성자: 김수인
+-- 작성일: 2026. 01. 21. 10:55:20
+
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
