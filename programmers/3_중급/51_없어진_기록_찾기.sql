eecfd24f5360f979230024af135da32a976058ec
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-02-26 09:50:04 +0900

    [프로그래머스] 중급 - 없어진 기록 찾기 (#51)

---
+++ programmers/3_중급/51_없어진_기록_찾기.sql
@@ -0,0 +1,14 @@
+-- 없어진 기록 찾기
+-- 프로그래머스 중급 (⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59042
+-- 작성자: 김수인
+-- 작성일: 2026. 02. 26. 09:50:03
+
+SELECT
+    O.ANIMAL_ID,
+    O.NAME
+FROM ANIMAL_OUTS AS O
+LEFT JOIN ANIMAL_INS AS I
+    ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE I.ANIMAL_ID IS NULL
+ORDER BY O.ANIMAL_ID ASC;
