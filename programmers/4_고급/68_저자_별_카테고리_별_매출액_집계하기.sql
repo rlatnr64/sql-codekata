17097194c54b6c4282457824a0bb9d739ec8af66
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-04-07 17:31:01 +0900

    [프로그래머스] 고급 - 저자 별 카테고리 별 매출액 집계하기 (#68)

---
+++ programmers/4_고급/68_저자_별_카테고리_별_매출액_집계하기.sql
@@ -0,0 +1,19 @@
+-- 저자 별 카테고리 별 매출액 집계하기
+-- 프로그래머스 고급 (⭐⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/144856
+-- 작성자: 김수인
+-- 작성일: 2026. 04. 07. 17:31:00
+
+SELECT 
+    B.AUTHOR_ID,
+    A.AUTHOR_NAME,
+    B.CATEGORY,
+    SUM(S.SALES * B.PRICE) AS TOTAL_SALES
+FROM BOOK_SALES AS S
+JOIN BOOK AS B
+    ON S.BOOK_ID = B.BOOK_ID
+JOIN AUTHOR AS A
+    ON B.AUTHOR_ID = A.AUTHOR_ID
+WHERE S.SALES_DATE LIKE '2022-01%'
+GROUP BY B.AUTHOR_ID, A.AUTHOR_NAME, B.CATEGORY
+ORDER BY B.AUTHOR_ID ASC, B.CATEGORY DESC;
